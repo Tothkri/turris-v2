2ba5666d589f866aa7ddd4b412f2fa469d98647e
Tóth Kristóf <sonxzf@inf.elte.hu>
2022-03-05 10:11:53 +0000

    Upload New File

---
+++ model/Mountain.java
@@ -0,0 +1,12 @@
+
+package model;
+
+import java.awt.Image;
+
+
+public class Mountain extends Sprite{
+
+    public Mountain(int x, int y, int height, int width, Image img) {
+        super(x, y, height, width, img);
+    }
+}
