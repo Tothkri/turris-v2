368f4a950680974712e70001f28a851b702522e3
Tóth Kristóf <sonxzf@inf.elte.hu>
2022-03-05 10:12:22 +0000

    Upload New File

---
+++ model/Lake.java
@@ -0,0 +1,12 @@
+
+package model;
+
+import java.awt.Image;
+
+
+public class Lake extends Sprite{
+
+    public Lake(int x, int y, int height, int width, Image img) {
+        super(x, y, height, width, img);
+    }
+}
