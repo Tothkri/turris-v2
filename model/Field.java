6bccfd669cdaf2855c90fa4f134b8082df7be9b4
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-05 10:11:45 +0000

    Upload New File

---
+++ model/Field.java
@@ -0,0 +1,12 @@
+
+package model;
+
+import java.awt.Image;
+
+
+public class Field extends Sprite{
+
+    public Field(int x, int y, int height, int width, Image img) {
+        super(x, y, height, width, img);
+    }
+}
