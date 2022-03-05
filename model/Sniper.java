aa1dd611e3e1c2429a17adf23380d3818e0de26b
Tóth Kristóf <sonxzf@inf.elte.hu>
2022-03-05 10:11:01 +0000

    Upload New File

---
+++ model/Sniper.java
@@ -0,0 +1,15 @@
+
+package model;
+
+import java.awt.Image;
+
+
+public class Sniper extends Tower{
+
+    public Sniper(String type, int power, int range, double attack_speed, int hp, int price, int x, int y, int height, int width, Image img) {
+        super(type, power, range, attack_speed, hp, price, x, y, height, width, img);
+    }
+    
+    
+    
+}
