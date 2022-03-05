ea6cd215a1cd3a3a8d329a0c64652b5acfa78499
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-05 10:11:54 +0000

    Upload New File

---
+++ model/Fighter.java
@@ -0,0 +1,16 @@
+package model;
+
+import java.awt.Image;
+import java.util.HashMap;
+
+
+public class Fighter extends Unit{
+
+    public Fighter(String type, int distance, int power, int hp, int price, HashMap<Integer, Integer> way, int x, int y, int height, int width, Image img) {
+        super(type, distance, power, hp, price, way, x, y, height, width, img);
+    }
+
+    
+    
+    
+}
