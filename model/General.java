526666ffabab7c43ac63479bffa1feccddb990db
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-05 10:12:17 +0000

    Upload New File

---
+++ model/General.java
@@ -0,0 +1,17 @@
+
+package model;
+
+import java.awt.Image;
+import java.util.HashMap;
+
+
+public class General extends Unit{
+
+    public General(String type, int distance, int power, int hp, int price, HashMap<Integer, Integer> way, int x, int y, int height, int width, Image img) {
+        super(type, distance, power, hp, price, way, x, y, height, width, img);
+    }
+
+    
+    
+    
+}
