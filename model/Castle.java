11950853a441bb2504dfb8d9696ec05d5151d506
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-05 10:11:08 +0000

    Upload New File

---
+++ model/Castle.java
@@ -0,0 +1,25 @@
+
+package model;
+
+import java.awt.Image;
+
+
+public class Castle extends Sprite{
+    
+    private int hp;
+
+    public Castle(int x, int y, int height, int width, Image img, int hp) {
+        super(x, y, height, width, img);
+        this.hp=hp;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+    
+    
+}
