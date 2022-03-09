d6a587ffaa96aa69133e7a564ad3ee0a92cefcd3
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-09 11:32:40 +0000

    Update Board.java

--- view/Board.java
+++ view/Board.java
@@ -1,17 +1,42 @@
 package view;
 
-import model.Model;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.util.ArrayList;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+import model.*;
 
-public class Board {
+public class Board extends JPanel{
     
-    private Model model;
+    private final Model model;
+    private final int width;
+    private final int height;
     
-    public Board(int selectedMap, String p1Name, String p2Name)
-    {
-        model = new Model(selectedMap, p1Name, p2Name);
+    public Board(int selectedMap, String p1Name, String p2Name, int width, int height){
+        model=new Model(selectedMap, p1Name, p2Name,height-150);
+        this.setPreferredSize(new Dimension(height-150,height-150));
+        this.width=width;
+        this.height=height;
     }
-    public Model getModel()
-    {
+    public Model getModel(){
         return model;
+    }
+  
+    
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        
+        //painting the whole board
+        super.paintComponent(grphcs);
+        Graphics2D g2 = (Graphics2D) grphcs;
+        grphcs.drawImage(new ImageIcon("src/res/Background.png").getImage(), 0, 0, height-150,height-150, null);
+        ArrayList<Sprite> terrain=model.getTerrain();
+       
+        for (Sprite s : terrain) {
+            
+            s.draw(g2);
+        }
     }
 }
