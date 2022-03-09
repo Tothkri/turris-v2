441162aa85755dd09795e33e7f6e0001c0cb7375
Tóth Kristóf <ypenkf@inf.elte.hu>
2022-03-09 11:35:39 +0000

    Update GameWindow.java

--- view/GameWindow.java
+++ view/GameWindow.java
@@ -1,54 +1,44 @@
 package view;
+import java.awt.Dimension;
 
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GameWindow extends JPanel {
+public class GameWindow extends  JPanel {
     
-    private final JButton teszt;
     private final Timer timer;
     private Board board;
     
     
-    public GameWindow(int width, int height)
+    public GameWindow(int width, int height, String p1Name, String p2Name, int selectedMap)
     {
         super();
         
-        teszt = new JButton("GameWindow button");
-        this.add(teszt);
-        timer = new Timer(1000 / 100, (ActionEvent ae) ->
-        {
-            
-        });
-    }
-    
-    public void setBoard(int selectedMap, String p1Name, String p2Name)
-    {
-        board = new Board(selectedMap, p1Name, p2Name);
-    }
-    
-    public JButton getTeszt()
-    {
-        return teszt;
-    }
-    
-    public void simulation()
-    {
+        board=new Board(selectedMap,p1Name,p2Name,width,height);
+        
+        this.add(board);
+        
+        setSize(width,height);
+        this.setPreferredSize(new Dimension(width,height));
+        
+         timer = new Timer(250, (ActionEvent ae) -> {
+          board.repaint();
+         });
         
     }
-    public void newRound()
-    {
+    
+    public void simulation(){
         
     }
-    public void gameOver()
-    {
+    public void newRound(){
         
     }
-    public void saveGame()
-    {
+    public void gameOver(){
+        
+    }
+    public void saveGame(){
         
     }
     
