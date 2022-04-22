b69ee9d1d51cdff562511f469149278f27e57deb
Tóth Kristóf <sonxzf@inf.elte.hu>
2022-04-22 17:41:12 +0000

    Update Sniper.java

--- src/model/Sniper.java
+++ src/model/Sniper.java
@@ -1,178 +1,59 @@
 package model;
 
-
-import java.awt.Color;
-import java.awt.Graphics2D;
 import java.awt.Image;
-import javax.swing.ImageIcon;
 
-public abstract class Tower extends Sprite {
+public class Sniper extends Tower {
 
-    protected String type;
-    protected int power;
-    protected int range;
-    protected double attackFrequency;
-    protected int hp;
-    protected int price;
-    protected int upgradePrice;
-    protected int demolishedIn;
-    protected int moneySpentOn;
-    protected int level;
-    protected int maxHp;
-    protected Color color;
-    protected Node shootCords;
-    protected boolean exploded;
-
-    public boolean isExploded() {
-        return exploded;
-    }
-
-    public void setExploded(boolean exploded) {
-        this.exploded = exploded;
-    }
-
-    public void setShootCords(int x, int y){
-        shootCords.setX(x);
-        shootCords.setY(y);
-    }
-
-    public Node getShootCords(){
-        return shootCords;
-    }
-
-
-    public Tower(String scolor, int x, int y, int height, int width, Image img) {
-        super(x, y, height, width, img);
-        exploded = false;
-        shootCords = new Node();
-        setShootCords(-1,-1);
+    //újonnan létrehozott
+    public Sniper(String scolor, int x, int y, int height, int width, Image img) {
+        super(scolor, x, y, height, width, img);
+        this.attackFrequency = 1.5;
+        this.hp = 25;
+        this.level = 1;
+        this.price = 300;
+        this.range = 1;
+        this.power = 2;
         this.demolishedIn = -1;
-        level = 1;
-        if (scolor == "red") {
-            this.color = Color.red;
-        } else {
-            this.color = Color.blue;
-        }
+        this.type = "Sniper";
+        this.maxHp = 25;
+        this.upgradePrice = price + 200;
+        this.moneySpentOn = price;
     }
 
-    public int getMoneySpentOn() {
-        return moneySpentOn;
-    }
-
-    public void setMoneySpentOn(int moneySpentOn) {
-        this.moneySpentOn = moneySpentOn;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
+    //fájból betöltött
+    public Sniper(String type, String sColor, int power, int range, double attackFrequency, int hp, int level, int x, int y, int height, int width, Image img) {
+        super(sColor, x, y, height, width, img);
         this.type = type;
-    }
-
-    public int getPower() {
-        return power;
-    }
-
-    public void setPower(int power) {
         this.power = power;
-    }
-
-    public int getRange() {
-        return range;
-    }
-
-    public void setRange(int range) {
         this.range = range;
-    }
-
-    public double getAttackFrequency() {
-        return attackFrequency;
-    }
-
-    public void setAttackFrequency(double attackFrequency) {
         this.attackFrequency = attackFrequency;
-    }
-
-    
-
-    public int getHp() {
-        return hp;
-    }
-
-    public void setHp(int hp) {
         this.hp = hp;
-    }
-
-    public int getPrice() {
-        return price;
-    }
-    
-    public void setColor(String colorString){
-        if (colorString.equals("red") ) {
-            this.color = Color.red;
+        this.level = level;
+        this.price = 100 + level * 200;
+        this.upgradePrice = price + 200;
+        this.maxHp = 75 + level * 25;
+        this.demolishedIn = -1;
+        this.maxHp = 20 + level * 5;
+        if (level == 1) {
+            this.moneySpentOn = 300;
+        } else if (level == 2) {
+            this.moneySpentOn = 800;
         } else {
-            this.color = Color.blue;
+            this.moneySpentOn = 1500;
         }
-    }
-
-    public void setPrice(int price) {
-        this.price = price;
-    }
-
-    public int getLevel() {
-        return level;
-    }
-    
-    public int getMaxHp() {
-        return maxHp;
-    }
-
-    public int getUpgradePrice() {
-        return upgradePrice;
-    }
-
-    public void setUpgradePrice(int upgradePrice) {
-        this.upgradePrice = upgradePrice;
-    }
-
-    public void setLevel(int lvl) {
-        level = lvl;
-    }
-
-    public abstract void upgrade();
-
-    public void demolish() {
-        demolishedIn = 4;//2 whole round
-        img = new ImageIcon("src/res/Destroyed.png").getImage();
-    }
-
-    public int getDemolishedIn() {
-        return demolishedIn;
-    }
-
-    public void setDemolishedIn(int x) {
-        demolishedIn -= x;
-    }
-
-    private double hpLine() {
-        double d = (double) hp / maxHp;
-            return d * width;
-
     }
 
     @Override
-    public void draw(Graphics2D g2) {
-        if(demolishedIn != -1) img = new ImageIcon("src/res/Destroyed.png").getImage();
-        g2.drawImage(img, x, y, height, width, null);
-        if (demolishedIn <=0) {
-            g2.setColor(color);
-            g2.drawLine(x + 2, y + height - 2, x + (int) hpLine() - 2, y + height - 2);
-            g2.drawLine(x + 2, y + height - 3, x + (int) hpLine() - 2, y + height - 3);
-            g2.drawLine(x + 2, y + height - 4, x + (int) hpLine() - 2, y + height - 4);
-        }
-
+    public void upgrade() {
+        attackFrequency -= 0.25;
+        power = 10;
+        hp += 5;
+        level++;
+        price = 100 + level * 200;
+        upgradePrice = price + 200;
+        maxHp = 20 + level * 5;
+        range++;
+        moneySpentOn += price;
     }
 
 }
